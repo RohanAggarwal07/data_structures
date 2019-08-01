c38e354664f620a73dd32f22a6da34989cc9177c
ROHANAGGARWAL07 <rohanaggarwal120@gmail.com>
2019-08-01 20:42:32 +0530

    Swapping the strings

---
+++ r.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+# include <string.h>
+using namespace std;
+int main(int argc,char*argv[]){
+cout<<argc<<endl;
+char *s;
+s=argv[1];
+argv[1]=argv[2];
+argv[2]=s;
+for(int i=1;i<argc;i++){
+cout<<argv[i]<<" ";}
+cout<<endl;
+return 0;}
